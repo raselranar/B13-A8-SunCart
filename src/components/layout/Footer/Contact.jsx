874118bac470867afe57d731e451e255851a4d75
Rasel <raselranar2005@gmail.com>
2026-05-01 00:46:29 +0600

    add icon in footer contact  section

--- src/components/layout/Footer/Contact.jsx
+++ src/components/layout/Footer/Contact.jsx
@@ -1,17 +1,24 @@
+import { LuMail, LuMapPin, LuPhone } from "react-icons/lu";
+
 const Contact = () => {
   return (
     <div>
       <h3 className="font-bold text-white text-lg mb-2">Contact Us</h3>
       <ul className="flex flex-col gap-2 *:text-gray-400">
-        <li className="text-sm">123 Summer Lane, Beach City, CA 90210</li>
-        <li>
+        <li className="text-sm flex items-center gap-1">
+          <LuMapPin className="text-amber-600" size={15} />
+          123 Summer Lane, Beach City, CA 90210
+        </li>
+        <li className="flex items-center gap-1">
+          <LuPhone className="text-amber-600" size={15} />
           <a
             href="tel:+18007862278"
             className="hover:text-accent text-sm transition-colors">
             +1 (800) 786-2278
           </a>
         </li>
-        <li>
+        <li className="flex items-center gap-1">
+          <LuMail className="text-amber-600" size={15} />
           <a
             href="mailto:hello@suncart.com"
             className="hover:text-accent text-sm transition-colors">
